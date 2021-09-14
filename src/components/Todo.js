a69599b7137e536929c0648b59ece09ae5528839
Fredrik <fredrikf2000@gmail.com>
2021-09-14 10:25:06 +0200

    added option to edit existing list items

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -3,11 +3,23 @@ import TodoForm from "./TodoForm";
 import { RiCloseCircleLine } from "react-icons/ri";
 import { TiEdit } from "react-icons/ti";
 
-const Todo = ({ todos, completeTodo, removeTodo }) => {
+const Todo = ({ todos, completeTodo, removeTodo, updateTodo }) => {
   const [edit, setEdit] = useState({
     id: null,
     value: "",
   });
+
+  const submitUpdate = (value) => {
+    updateTodo(edit.id, value);
+    setEdit({
+      id: null,
+      value: "",
+    });
+  };
+
+  if (edit.id) {
+    return <TodoForm edit={edit} onSubmit={submitUpdate} />;
+  }
 
   return todos.map((todo, index) => (
     <div
@@ -22,7 +34,10 @@ const Todo = ({ todos, completeTodo, removeTodo }) => {
           onClick={() => removeTodo(todo.id)}
           className="delete-icon"
         />
-        <TiEdit onClick={() => setEdit(todo.text)} className="edit-icon" />
+        <TiEdit
+          onClick={() => setEdit({ id: todo.id, value: todo.text })}
+          className="edit-icon"
+        />
       </div>
     </div>
   ));
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -15,6 +15,16 @@ const TodoList = () => {
     setTodos(newTodos);
   };
 
+  const updateTodo = (todoId, newValue) => {
+    if (!newValue.text || /^\s*$/.test(newValue.text)) {
+      return;
+    }
+
+    setTodos((prev) =>
+      prev.map((item) => (item.id === todoId ? newValue : item))
+    );
+  };
+
   const removeTodo = (id) => {
     const removeArr = [...todos].filter((todo) => todo.id !== id);
 
@@ -35,7 +45,12 @@ const TodoList = () => {
     <div>
       <h1>What's the plan for today?</h1>
       <TodoForm onSubmit={addTodo} />
-      <Todo todos={todos} completeTodo={completeTodo} removeTodo={removeTodo} />
+      <Todo
+        todos={todos}
+        completeTodo={completeTodo}
+        removeTodo={removeTodo}
+        updateTodo={updateTodo}
+      />
     </div>
   );
 };
